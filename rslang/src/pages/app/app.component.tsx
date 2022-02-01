e3be23da7c02642575f5d2ccafbe61464c5348e0
Daria Malina <gluto68@gmail.com>
2022-02-01 14:27:38 +0300

    fix: redirected to correct folder

--- rslang/src/pages/app/app.component.tsx
+++ rslang/src/pages/app/app.component.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import GlobalStyle from './styles';
 import { hot } from 'react-hot-loader/root';
 import { Footer } from '../../features/footer';
-import { Account } from '../../features/account/account.component';
+import { Account } from '../../components/account/account.component';
 import Header from '../../features/header/header.component';
 import { BrowserRouter } from 'react-router-dom';
 
