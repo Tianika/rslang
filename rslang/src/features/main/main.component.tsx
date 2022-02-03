46447ba3b40ce13e7b50b89b7d353bd7b92c6555
nobodynoticed <eugenejukov23@gmail.com>
2022-02-03 17:18:29 +0300

    init: start main component implement

---
+++ rslang/src/features/main/main.component.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const Main: FC = () => {
+  return (
+    <main>
+      <section>
+        <h1>test</h1>
+      </section>
+    </main>
+  );
+};
+
+export default Main;
--- rslang/src/pages/app/app.component.tsx
+++ rslang/src/pages/app/app.component.tsx
@@ -4,6 +4,7 @@ import { hot } from 'react-hot-loader/root';
 import { Footer } from '../../features/footer';
 import { Account } from '../../components/account';
 import Header from '../../features/header/header.component';
+import { Main } from '../../features/main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Statistic } from '../../components/statistic/statistic.component';
 import { Signup } from '../../components/account/signup.component';
@@ -18,6 +19,7 @@ const App: React.FC = () => {
         <Route path="/account/login" element={<Account />} />
         <Route path="/account/signup" element={<Signup />} />
       </Routes>
+      <Main />
       <Footer />
     </BrowserRouter>
   );
