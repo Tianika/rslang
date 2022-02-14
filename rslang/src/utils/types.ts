fa7f7538ed43e2f7eeb3864571c98d6b4c646dc8
Tianika <tatyriana@yandex.ru>
2022-02-14 21:00:35 +0300

    feat: add audio for result words

--- rslang/src/utils/types.ts
+++ rslang/src/utils/types.ts
@@ -1,6 +1,8 @@
 type ChangeEventHandle = (value: React.ChangeEvent<HTMLInputElement>) => void;
 type ChangeSelectHandle = (value: React.ChangeEvent<HTMLSelectElement>) => void;
 
+export type AudioEventHandle = (value: React.SyntheticEvent<HTMLElement>) => void;
+
 export type AccountProps = {
   onNameChange: ChangeEventHandle;
   onEmailChange: ChangeEventHandle;
