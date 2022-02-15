d4485693c9d5aeb839e7ddeb82e7934984e77e7b
Daria Malina <gluto68@gmail.com>
2022-02-15 17:26:45 +0300

    feat: add api, slice, saga

---
+++ rslang/src/features/audio-games/audio-call.saga.ts
@@ -0,0 +1,47 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import { createAction, PayloadAction } from '@reduxjs/toolkit';
+import * as Effects from 'redux-saga/effects';
+import { LoadingState } from '../../utils';
+import { requestWordsFromGroup } from './audio-call.api';
+import { getRandomNumber } from './utils';
+//import { sprintStartActions } from './sprint.slice';
+import { sprintGameActions } from './audio-call.slice';
+
+const call: any = Effects.call;
+
+//создаем экшен для запроса
+export const fetchSprintAction = createAction<number, string>('sprint/fetch');
+
+//получаем функцию из экшенов
+const { changeLoadingState, setWordsArray } = sprintGameActions;
+
+function* sprintGameFetch(action: PayloadAction<number>) {
+  yield put(changeLoadingState(LoadingState.Loading));
+
+  try {
+    //рандомный номер страницы
+    const pageNumber = getRandomNumber(30);
+
+    //получаем данные из запроса
+    const { data } = yield call(requestWordsFromGroup, action.payload, pageNumber) as Response;
+
+    //обрабатываем полученный массив слов
+    yield put(setWordsArray(data));
+
+    //TODO доделать loading
+
+    yield put(changeLoadingState(LoadingState.Success));
+  } catch (error: any) {
+    console.log(error.response.status);
+    //TODO добавить обработку ошибок???
+
+    yield put(changeLoadingState(LoadingState.Error));
+  }
+}
+
+function* sprintSaga() {
+  yield takeLatest(fetchSprintAction, sprintGameFetch);
+}
+
+export default sprintSaga;
+//добавить в rootsaga
---
+++ rslang/src/features/audio-games/audio-call.slice.ts
@@ -0,0 +1,39 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { LoadingState } from '../../utils';
+import { SprintWordsState } from './types';
+
+const initialState: SprintWordsState = {
+  loadingState: LoadingState.Initial,
+  words: [],
+  rightAnswers: [],
+  errorAnswers: []
+};
+
+//создаем редюсеры
+export const sprintGameSlice = createSlice({
+  name: 'sprint/game',
+  initialState,
+  reducers: {
+    changeLoadingState: (state, action) => {
+      state.loadingState = action.payload;
+    },
+    setWordsArray: (state, action) => {
+      state.words = [...action.payload];
+    },
+    addRightAnswers: (state, action) => {
+      state.rightAnswers.push(action.payload);
+    },
+    addErrorAnswers: (state, action) => {
+      state.errorAnswers.push(action.payload);
+    },
+    resetAnswerArrays: (state) => {
+      state.rightAnswers = [];
+      state.errorAnswers = [];
+    }
+  }
+});
+
+export const sprintGameReducer = sprintGameSlice.reducer;
+//добавить sprintGameSlice в reducer.ts
+
+export const sprintGameActions = sprintGameSlice.actions;
