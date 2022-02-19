ebd25ee8e4716f6b9a250c368017a986f06299ab
Daria Malina <gluto68@gmail.com>
2022-02-19 16:28:46 +0300

    feat: add audioSaga in rootSaga

--- rslang/src/app/saga.ts
+++ rslang/src/app/saga.ts
@@ -1,9 +1,8 @@
 import { all } from 'redux-saga/effects';
 import loginSaga from '../features/login/login.saga';
 import signupSaga from '../features/signup/signup.saga';
-//import sprintSaga from '../features/sprint/sprint.saga';
+import audioSaga from '../features/audio-games/audio-call.saga';
 
 export function* rootSaga() {
-  //), sprintSaga(
-  yield all([loginSaga(), signupSaga()]);
+  yield all([loginSaga(), signupSaga(), audioSaga()]);
 }
