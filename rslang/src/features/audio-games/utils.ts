29d4be469c2d3a66315fcca91587cd0242bf2e70
Daria Malina <gluto68@gmail.com>
2022-02-19 16:30:15 +0300

    feat: add function shuffle array

--- rslang/src/features/audio-games/utils.ts
+++ rslang/src/features/audio-games/utils.ts
@@ -1,3 +1,6 @@
 export const getRandomNumber = (maxNumber: number): number => {
   return Math.floor(Math.random() * maxNumber);
 };
+export const ShuffleArray = (array: string[]): string[] => {
+  return array.sort(() => Math.random() - 0.5);
+};
