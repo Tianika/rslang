c304999fa08b3181700c987b845bbd08b45f2972
Daria Malina <gluto68@gmail.com>
2022-02-21 21:15:51 +0300

    feat: add error message (for unauthorized users)

--- rslang/src/pages/statistics/statistics.component.tsx
+++ rslang/src/pages/statistics/statistics.component.tsx
@@ -1,5 +1,10 @@
 import { Table } from '../../components/table/table.component';
+import { UnauthorizedUserStatistic } from '../../components/unauthorizedUserStatistic/unauthorizedUserStatistic.component';
 
 export const Statistics = () => {
-  return <Table />;
+  if (localStorage.rslangUserId) {
+    return <Table />;
+  } else {
+    return <UnauthorizedUserStatistic />;
+  }
 };
