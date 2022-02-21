f690c33ff669477e81cd3a199063f489656371bc
Daria Malina <gluto68@gmail.com>
2022-02-21 16:44:04 +0300

    feat: add page loading

--- rslang/src/features/audio-games/game-window.component.tsx
+++ rslang/src/features/audio-games/game-window.component.tsx
@@ -19,10 +19,13 @@ import {
 import { fetchAudioAction } from './audio-call.saga';
 import { audioGameActions } from './audio-call.slice';
 import { ResultGamePage } from '../result-game';
-import { GameTypes } from '../../utils';
+import { GameTypes, LoadingState } from '../../utils';
+import { LoadingPage } from '../../components/loading';
+import { loadingStatus } from '../sprint/sprint.selectors';
 
 const { addRightAnswers, addErrorAnswers, resetAnswerArrays } = audioGameActions;
 const GameWindow = (props: { level: number }): React.ReactElement => {
+  const status = useAppSelector(loadingStatus);
   const urlQuery = 'https://learnwords-team17.herokuapp.com/';
   const dispatch = useAppDispatch();
   const words = useAppSelector(wordsSelector);
@@ -40,7 +43,18 @@ const GameWindow = (props: { level: number }): React.ReactElement => {
   const [currentLongestSeries, setCurrentLongestSeries] = useState(0);
   const [getAnswerButtonClick, setGetAnswerButtonClick] = useState(true);
   const arrayWordRightId = [];
+  const [isLoading, setIsLoading] = useState(true);
+  const disableIsLoading = () => {
+    setIsLoading(false);
+  };
 
+  useEffect(() => {
+    if (status === LoadingState.Success) {
+      disableIsLoading();
+    }
+  }, [status]);
+
+  //пока не догрузились данные страница Loading
   useEffect(() => {
     dispatch(fetchAudioAction(props.level));
     dispatch(resetAnswerArrays());
@@ -57,7 +71,7 @@ const GameWindow = (props: { level: number }): React.ReactElement => {
       setCurrentImage(urlQuery + word.image);
     }
   }, [words]);
-
+  if (isLoading) return <LoadingPage />;
   const upCurrentWordIndex = () => {
     setCurrentWordIndex(currentWordIndex + 1);
   };
