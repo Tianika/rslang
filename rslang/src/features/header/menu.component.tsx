51c343cd4df7d114e1723bb72767192ec33044ff
Daria Malina <gluto68@gmail.com>
2022-03-15 17:42:37 +0300

    fix: burger close click

--- rslang/src/features/header/menu.component.tsx
+++ rslang/src/features/header/menu.component.tsx
@@ -5,10 +5,18 @@ import React from 'react';
 const Menu = ({ open, setOpen }: { open: boolean; setOpen?: any }) => {
   return (
     <StyledMenu open={open}>
-      <Link to="/">Главная</Link>
-      <Link to="/textbook">Учебник</Link>
-      <Link to="/games">Мини-игры</Link>
-      <Link to="/statistic">Статистика</Link>
+      <Link to="/" onClick={() => setOpen(!open)}>
+        Главная
+      </Link>
+      <Link to="/textbook" onClick={() => setOpen(!open)}>
+        Учебник
+      </Link>
+      <Link to="/games" onClick={() => setOpen(!open)}>
+        Мини-игры
+      </Link>
+      <Link to="/statistic" onClick={() => setOpen(!open)}>
+        Статистика
+      </Link>
     </StyledMenu>
   );
 };
