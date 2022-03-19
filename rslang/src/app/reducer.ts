718ac59d189eca5cfb26df2d5b93752a58744fac
Daria Malina <gluto68@gmail.com>
2022-03-19 12:21:22 +0300

    refactor: delete saga+api

--- rslang/src/app/reducer.ts
+++ rslang/src/app/reducer.ts
@@ -4,15 +4,13 @@ import { signupReducer } from '../features/signup/signup.slice';
 import { sprintGameReducer } from '../features/sprint/sprint.slice';
 import { wordsPageReducer } from '../features/wordsPage/wordsPage.slice';
 import { audioGameReducer } from '../features/audio-games/audio-call.slice';
-import { getStatisticReducer } from '../components/statistic-data/statistic-data.slice';
 
 const rootReducer = combineReducers({
   login: loginReducer,
   signup: signupReducer,
   sprintGame: sprintGameReducer,
   wordsPage: wordsPageReducer,
-  audioGame: audioGameReducer,
-  statistics: getStatisticReducer
+  audioGame: audioGameReducer
 });
 
 export { rootReducer };
