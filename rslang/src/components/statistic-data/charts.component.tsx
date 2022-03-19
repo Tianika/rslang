6ac62247dcdaae3caec01e6233280ea70e3c7391
Daria Malina <gluto68@gmail.com>
2022-03-19 13:34:23 +0300

    feat: add charts + data

--- rslang/src/components/statistic-data/charts.component.tsx
+++ rslang/src/components/statistic-data/charts.component.tsx
@@ -1,9 +1,33 @@
 import React, { useEffect, useState } from 'react';
-// import { Doughnut } from 'react-chartjs-2';
-// import { Line } from 'react-chartjs-2';
 import axios from 'axios';
+import { ChartBlock } from './style';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top' as const
+    },
+    title: {
+      display: true,
+      text: 'Долгосрочная статистика'
+    }
+  }
+};
 
 const Charts = (): React.ReactElement => {
+  let labels;
+  const [data, setData] = useState({});
   const config = {
     headers: {
       Authorization: `Bearer ${localStorage.rslangUserToken}`
@@ -14,6 +38,34 @@ const Charts = (): React.ReactElement => {
     `https://learnwords-team17.herokuapp.com/users/${id}/statistics`,
     config
   );
-  return <div>Долгосрочная статистика</div>;
+  useEffect(() => {
+    request.then(function (response) {
+      if (response.status === 200) {
+        labels = Object.keys(response.data.optional.long);
+        const dataDay: any = Object.values(response.data.optional.long);
+        setData({
+          labels,
+          datasets: [
+            {
+              label: 'Новые слова',
+              data: labels.map((el, i) => dataDay[i].newWord),
+              backgroundColor: 'rgba(245,255,99,0.5)'
+            },
+            {
+              label: 'Всего слов',
+              data: labels.map((el, i) => dataDay[i].learnedWords),
+              backgroundColor: 'rgba(53, 162, 235, 0.5)'
+            }
+          ]
+        });
+      }
+    });
+  }, []);
+  return (
+    <ChartBlock>
+      <Bar options={options} data={data} />
+      <div>график</div>
+    </ChartBlock>
+  );
 };
 export default Charts;
--- rslang/src/components/statistic-data/style.ts
+++ rslang/src/components/statistic-data/style.ts
@@ -5,23 +5,24 @@ export const TableStatistic = styled.div`
   border: none;
   border-collapse: collapse;
   margin: 100px auto;
-  @media (max-width: 1000px) {
-    margin: 20px auto;
+  @media (max-width: 500px) {
+    margin: 20px 0;
   }
 `;
-export const TableStatisticTitle = styled.div`
+export const TableStatisticTitle = styled.h2`
   margin-bottom: 30px;
   font-size: 30px;
-  @media (max-width: 1000px) {
-    width: 90%;
+  font-weight: 100;
+  @media (max-width: 500px) {
+    font-size: 20px;
   }
 `;
 export const TableStatisticHead = styled.div`
   display: table-header-group;
   vertical-align: middle;
   border-color: inherit;
-  @media (max-width: 1000px) {
-    width: 90%;
+  @media (max-width: 500px) {
+    width: 300px;
   }
 `;
 export const TableStatisticHeadTr = styled.div`
@@ -45,6 +46,9 @@ export const TableStatisticHeadThStart = styled.div`
   border-radius: 8px 0 0 8px;
   letter-spacing: 0.01071em;
   vertical-align: inherit;
+  @media (max-width: 1000px) {
+    font-size: 12px;
+  }
 `;
 export const TableStatisticHeadThEnd = styled.div`
   color: rgba(0, 0, 0, 0.87);
@@ -58,6 +62,9 @@ export const TableStatisticHeadThEnd = styled.div`
   border-radius: 0 8px 8px 0;
   letter-spacing: 0.01071em;
   vertical-align: inherit;
+  @media (max-width: 1000px) {
+    font-size: 12px;
+  }
 `;
 export const TableStatisticHeadTh = styled.div`
   color: rgba(0, 0, 0, 0.87);
@@ -70,6 +77,9 @@ export const TableStatisticHeadTh = styled.div`
   font-family: 'Roboto', sans-serif;
   letter-spacing: 0.01071em;
   vertical-align: inherit;
+  @media (max-width: 1000px) {
+    font-size: 12px;
+  }
 `;
 export const TableStatisticBody = styled.div`
   display: table-row-group;
@@ -103,6 +113,9 @@ export const TableStatisticBodyTh = styled.div`
   letter-spacing: 0.01071em;
   vertical-align: inherit;
   text-align: center;
+  @media (max-width: 1000px) {
+    font-size: 12px;
+  }
 `;
 export const TableStatisticBodyThName = styled.div`
   border-radius: 8px 0 0 8px;
@@ -116,4 +129,25 @@ export const TableStatisticBodyThName = styled.div`
   line-height: 1.43;
   letter-spacing: 0.01071em;
   vertical-align: inherit;
+  @media (max-width: 1000px) {
+    font-size: 12px;
+  }
+`;
+export const ChartBlock = styled.div`
+  width: 80%;
+  border: none;
+  border-collapse: collapse;
+  margin: 100px auto;
+  @media (max-width: 500px) {
+    margin: 20px 0;
+  }
+`;
+
+export const ChartTitle = styled.h2`
+  margin-bottom: 30px;
+  font-size: 30px;
+  font-weight: 100;
+  @media (max-width: 500px) {
+    font-size: 20px;
+  }
 `;
